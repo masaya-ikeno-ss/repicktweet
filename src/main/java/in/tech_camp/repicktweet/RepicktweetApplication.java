b3d5e2418b04768d4c417c217eee8a19dfd07d04
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-05-19 10:23:04 +0900

    re_picktweetの初期設定

---
+++ src/main/java/in/tech_camp/repicktweet/RepicktweetApplication.java
@@ -0,0 +1,13 @@
+package in.tech_camp.repicktweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepicktweetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepicktweetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/tech_camp/repicktweet/RepicktweetApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tech_camp.repicktweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepicktweetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

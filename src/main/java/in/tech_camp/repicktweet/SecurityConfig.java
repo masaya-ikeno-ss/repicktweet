f5bb0aaa3f3be5ca0fa630c27c5838a01ff56f35
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-05-19 14:16:15 +0900

    ログインの有無でヘッダーの内容を変更

--- src/main/java/in/tech_camp/repicktweet/SecurityConfig.java
+++ src/main/java/in/tech_camp/repicktweet/SecurityConfig.java
@@ -19,19 +19,30 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/css/**", "/images/**", "/", "/users/sign_up", "/users/login").permitAll()
+                        //ここに記述されたGETリクエストは許可されます（ログイン不要です)
+                        .requestMatchers("/css/**", "/images/**", "/", "/users/sign_up", "/users/login", "/tweets/{id:[0-9]+}").permitAll()
+                        //ここに記述されたPOSTリクエストは許可されます(ログイン不要です)
                         .requestMatchers(HttpMethod.POST, "/user").permitAll()
+                        //上記以外のリクエストは認証されたユーザーのみ許可されます(要ログイン)
                         .anyRequest().authenticated())
+
                 .formLogin(login -> login
+                        //ログインフォームでログインボタンを押した際のパスを設定しています
                         .loginProcessingUrl("/login")
+                        //ログインフォームのパスを設定しています
                         .loginPage("/users/login")
+                        //ログイン成功後のリダイレクト先です
                         .defaultSuccessUrl("/", true)
+                        //ログイン失敗後のリダイレクト先です
                         .failureUrl("/login?error")
+                        //ログイン時にusernameとして扱うパラメーターを指定します
                         .usernameParameter("email") 
                         .permitAll())
 
                 .logout(logout -> logout
+                        //ログアウトボタンを押した際のパスを設定しています
                         .logoutUrl("/logout")
+                        //ログアウト成功時のリダイレクト先です
                         .logoutSuccessUrl("/"));
 
         return http.build();

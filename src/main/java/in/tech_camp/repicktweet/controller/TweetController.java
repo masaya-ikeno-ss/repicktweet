d229425112378d48f0b631529c73eac8fb5e3925
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-06-06 16:52:29 +0900

    削除機能の準備

--- src/main/java/in/tech_camp/repicktweet/controller/TweetController.java
+++ src/main/java/in/tech_camp/repicktweet/controller/TweetController.java
@@ -11,8 +11,10 @@ import java.util.List;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -79,4 +81,14 @@ public class TweetController {
     return "redirect:/";
   }
   
+  @DeleteMapping("/tweets/{tweetId}/delete")
+  public String delete(@PathVariable("tweetId") Integer tweetId) {
+    try {
+      tweetRepository.deleteTweet(tweetId);
+    } catch (Exception e) {
+      System.out.println("エラー：" + e);
+      return "redirect:/";
+    }
+    return "redirect:/";
+  }
 }
